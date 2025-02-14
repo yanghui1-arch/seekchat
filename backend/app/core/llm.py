ebff8a3c83fb1f2b5e4f67d2ec730a984e3101c7
dass90 <74724408+yanghui1-arch@users.noreply.github.com>
2025-02-14 14:19:49 +0800

    Update llm.py

--- backend/app/core/llm.py
+++ backend/app/core/llm.py
@@ -1,2 +1,2 @@
-DEEPSEEK_BASE_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1'
-DEEPSEEK_API_KEY = 'sk-af1b9aba64db4b3a91d827bb98e8cd8b'
+DEEPSEEK_BASE_URL = '<DEEPSEEK_BASE_URL>' # change!
+DEEPSEEK_API_KEY = '<YOUR_DEEPSEEK_API_KEY>' # change to your api key
